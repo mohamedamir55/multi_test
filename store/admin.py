0c60f01eb5c6056e12594a4554a90919a90b362f
mohamedamir55 <mohamedamir618@gmail.com>
2024-06-01 18:10:12 +0300

    model of store

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from.models import Category,Color,Size,ImagePruduct,Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display='name','slug'
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display= 'name','slug'
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display= 'name','slug'
+
+@admin.register(ImagePruduct)
+class ImagePruductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+    
+
+
+    
+
+
+    
+
+    
+
