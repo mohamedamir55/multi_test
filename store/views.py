41c93a55cb6122777f8af0c010ddd71d4c2ca609
mohamedamir55 <mohamedamir618@gmail.com>
2024-07-02 14:16:48 +0300

    update detail

--- store/views.py
+++ store/views.py
@@ -16,5 +16,15 @@ def store (request):
 def product_detail(request,category_slug,slug):
     # product=Product.objects.get(slug=slug)
     product=get_object_or_404(Product,category__slug=category_slug,slug=slug)
+    images=ImagePruduct.objects.filter(Product=product)
+    sizes= product.size.all()
+    colors=product.color.all()
 
-    return render (request,'store/product_detail.html')
+
+    context={
+        'product':product,
+        'images':images ,
+        'sizes':sizes ,
+        'colors':colors,
+    }
+    return render (request,'store/product_detail.html',context)
